47b7ca5beaf12e6e353e17d3c7cb2a07d2254fa3
CHEUK WA CHRISTOPHER LO <cheuk.wa.lo@avantcorp.com>
2017-07-07 17:37:31 +0900

    F

---
+++ FlowerMeanings/ContentsFileIO.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace FlowerMeanings
+{
+    static class ContentsFileIO
+    {
+        //csv reading
+        public static Dictionary<string,string> Read()
+        {
+            Dictionary<string,string> dictionary = new Dictionary<string, string>();
+
+            if (File.Exists(@"Data\flowers.csv") == false)
+            {
+                return dictionary;
+            }
+
+            try
+            {
+                using (StreamReader reader = new StreamReader
+                    (@"Data\flowers.csv", Encoding.Default))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        string[] c = line.Split(',');
+                        dictionary.Add(c[0],c[1]);
+                    }
+                }
+                return dictionary;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return dictionary;
+            }
+        }
+
+        //csv writing
+        public static void Write(List<string> list)
+        {
+            if (Directory.Exists("Data") == false)
+            {
+                Directory.CreateDirectory("Data");
+            }
+
+            try
+            {
+                using (StreamWriter writer = new StreamWriter
+                    (@"Data\flowers.csv", false, Encoding.Default))
+                {
+                    foreach (string s in list)
+                    {
+                        writer.Write(s);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
---
+++ FlowerMeanings/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace FlowerMeanings
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void buttonSearch_Click(object sender, EventArgs e)
+        {
+            if (Flower.Get(textBoxName.Text, comboBoxColour.Text) != null)
+            {
+                if (comboBoxColour.Text == "")
+                    MessageBox.Show(textBoxName.Text + "の花言葉は" + Flower.Get(textBoxName.Text, comboBoxColour.Text) + "です。", "花言葉");
+                else
+                    MessageBox.Show(textBoxName.Text + "(" + comboBoxColour.Text + ")の花言葉は" + Flower.Get(textBoxName.Text, comboBoxColour.Text) + "です。", "花言葉");
+            }
+
+        }
+
+        private void buttonEdit_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2();
+            form2.ShowDialog();
+            form2.Dispose();
+        }
+    }
+}
